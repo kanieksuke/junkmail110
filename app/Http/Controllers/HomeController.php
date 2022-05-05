1714f5648a23d0ee9b4e2c81f7fb4a88b5cd585b
kanieksuke <kanieksuke@yahoo.co.jp>
2022-05-05 13:10:48 +0900

    indexメソッド修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $posts = Post::where('status', 1)->orderBy('updated_at', 'desc')->get();
+        return view('index', compact('posts'));
     }
 
     public function create()
