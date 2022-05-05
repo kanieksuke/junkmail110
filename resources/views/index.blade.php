76fdd1bf2435fd1884cf6fee919c1e2bc2af04a5
kanieksuke <kanieksuke@yahoo.co.jp>
2022-05-05 13:34:59 +0900

    画面に投稿データを表示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                @foreach($posts AS $post)
+                    <div class="card-header">
+                        {{ $post['title'] }}
+                    </div>
+                    <div class="card-body">
+                            <div class="alert alert-success" role="alert">
+                                {{ $post['image'] }}
+                            </div>
+                        <!-- You are logged in! -->
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
