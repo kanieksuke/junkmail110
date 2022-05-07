325635e4d69a4f9de9f2f7654ecb0050b8e4dd46
kanieksuke <kanieksuke@yahoo.co.jp>
2022-05-07 17:18:43 +0900

    ユーザー名表示

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    {{ $post['title'] }}
+                    {{ $post['title'] }} by {{ $post->user->name }}
                 </div>
                 <div class="card-body">
                     <div class="alert alert-success">
