5cc9d92707c1e187e38d13a87f82a123de35c9fe
kanieksuke <kanieksuke@yahoo.co.jp>
2022-05-07 13:07:09 +0900

    画像データ取得

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,16 @@
                         {{ $post['title'] }}
                     </div>
                     <div class="card-body">
-                            <div class="alert alert-success" role="alert">
-                                <img src="{{ asset('storage/image/' . $post->image) }}" />
+                            <div class="alert alert-success">
+                                @if(file_exists(public_path().'/storage/image/'. $post->id .'.jpg'))
+                                    <img src="/storage/image/{{ $post->id }}.jpg" width=300px/>
+                                @elseif(file_exists(public_path().'/storage/image/'. $post->id .'.jpeg'))
+                                    <img src="/storage/image/{{ $post->id }}.jpeg" width=300px/>
+                                @elseif(file_exists(public_path().'/storage/image/'. $post->id .'.png'))
+                                    <img src="/storage/image/{{ $post->id }}.png" width=300px/>
+                                @elseif(file_exists(public_path().'/storage/image/'. $post->id .'.gif'))
+                                    <img src="/storage/image/{{ $post->id }}.gif" width=300px/>
+                                @endif
                             </div>
                         <!-- You are logged in! -->
                     </div>
