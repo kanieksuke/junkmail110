2a42d106ef5a8da5d776a2f04440e1cef93dd044
kanieksuke <kanieksuke@yahoo.co.jp>
2022-05-19 19:31:25 +0900

    editメソッド定義

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -82,4 +82,11 @@ class HomeController extends Controller
         $comments = Comment::where('status', 1)->orderBy('updated_at', 'desc')->get();
         return view('show',compact('post'));
     }
+
+    public function edit($id){
+        $user = \Auth::user();
+        $post = Post::where('status', 1)->where('id', $id)->where('user_id', $user['id'])
+            ->first;
+        return view('edit',compact('user'));
+    }
 }
